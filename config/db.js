dd2ef79835be26472b670199c4ace1082b18eb08
Jidnyesh <jidnyeshjawanjal29@gmail.com>
2022-05-26 13:21:35 +0530

    New DB

--- config/db.js
+++ config/db.js
@@ -2,11 +2,17 @@ const mysql = require('mysql')
 require("dotenv").config()
 
 
+// const db = mysql.createConnection({
+// host: "chess-be.ctwicvo7fb5e.ap-south-1.rds.amazonaws.com",
+// user: "admin",
+// password: "ChessBE123",
+// database:"Chess" 
+// })
 const db = mysql.createConnection({
-host: "chess-be.ctwicvo7fb5e.ap-south-1.rds.amazonaws.com",
-user: "admin",
-password: "ChessBE123",
-database:"Chess" 
-})
+    host: "chess.cp6fonzwfmav.ap-south-1.rds.amazonaws.com",
+    user: "admin",
+    password: "ChessBE123",
+    database:"Chess" 
+    })
 
 module.exports = db;
